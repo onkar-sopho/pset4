6f634c2c9f183073a0c91f20dca5411b7dffa783
Onkar Rai <onkar21.rai@gmail.com>
2016-12-25 06:39:21 +0000

    Enhancing in progress....still more clarity needed

--- bmp/whodunit.c
+++ bmp/whodunit.c
@@ -1,10 +1,10 @@
 /**
- * copy.c
+ * whodunit.c
  *
  * Computer Science 50
  * Problem Set 4
  *
- * Copies a BMP piece by piece, just because.
+ * This file is a solution to the whodunnit-Problem of pset4
  */
        
 #include <stdio.h>
@@ -17,59 +17,59 @@ int main(int argc, char* argv[])
     // ensure proper usage
     if (argc != 3)
     {
-        printf("Usage: ./copy infile outfile\n");
+        printf("Usage: ./whodunit clue verdict\n");
         return 1;
     }
 
     // remember filenames
-    char* infile = argv[1];
-    char* outfile = argv[2];
+    char* clue = argv[1];
+    
 
-    // open input file 
-    FILE* inptr = fopen(infile, "r");
-    if (inptr == NULL)
+    // open clue file 
+    FILE* clue_ptr = fopen(clue , "r");
+    if (clue_ptr == NULL)
     {
-        printf("Could not open %s.\n", infile);
+        printf("Could not open %s.\n", clue);
         return 2;
     }
 
-    // open output file
-    FILE* outptr = fopen(outfile, "w");
-    if (outptr == NULL)
+    // open verdict file
+    FILE* verdict_ptr = fopen("verdict.bmp", "w");
+    if (verdict_ptr == NULL)
     {
-        fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", outfile);
+        fclose(clue_ptr);
+        fprintf(stderr, "Could not create %s.\n", "verdict.bmp");
         return 3;
     }
 
-    // read infile's BITMAPFILEHEADER
+    // read clue file's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, clue_ptr);
 
-    // read infile's BITMAPINFOHEADER
+    // read clue file's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, clue_ptr);
 
-    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    // ensure clue file is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
-        fclose(outptr);
-        fclose(inptr);
+        fclose(verdict_ptr);
+        fclose(clue_ptr);
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
 
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    // write verdict file's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, verdict_ptr);
 
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    // write verdict file's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, verdict_ptr);
 
     // determine padding for scanlines
     int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
-    // iterate over infile's scanlines
+    // iterate over clue file's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
         // iterate over pixels in scanline
@@ -78,28 +78,38 @@ int main(int argc, char* argv[])
             // temporary storage
             RGBTRIPLE triple;
 
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            // read RGB triple from clue file
+            fread(&triple, sizeof(RGBTRIPLE), 1, clue_ptr);
+            
+            if (triple.rgbtRed == 0xff)
+              triple.rgbtRed = 0x00;
+            
+            if (triple.rgbtRed == 0x00 && triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00)
+            {
+              triple.rgbtRed = 0xff;
+              triple.rgbtGreen = 0xff;
+              triple.rgbtBlue = 0xff;
+            } 
+            
+            // write RGB triple to  verdict file
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, verdict_ptr);
         }
 
         // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
+        fseek(clue_ptr, padding, SEEK_CUR);
 
         // then add it back (to demonstrate how)
         for (int k = 0; k < padding; k++)
         {
-            fputc(0x00, outptr);
+            fputc(0x00, verdict_ptr);
         }
     }
 
-    // close infile
-    fclose(inptr);
+    // close clue file
+    fclose(clue_ptr);
 
-    // close outfile
-    fclose(outptr);
+    // close verdict file
+    fclose(verdict_ptr);
 
     // that's all folks
     return 0;
