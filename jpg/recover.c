d0f66cbf8bdfcf62b99e07345fa36944f64e9a86
Onkar Rai <onkar21.rai@gmail.com>
2016-12-25 07:36:13 +0000

    Finished implementing recover.c...all 50 jpegs recovered perfectly

--- jpg/recover.c
+++ jpg/recover.c
@@ -7,7 +7,71 @@
  * Recovers JPEGs from a forensic image.
  */
 
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t BYTE;
+
+int main(void)
 {
-    // TODO
+    // Open the memory card file
+    FILE* fp = fopen("card.raw", "r");
+    
+    // Check file permission
+    if (fp == NULL)
+    {
+        printf("Could not recover JPEGS from the card.\n");
+        return 1;
+    }
+    
+    // Open output file 
+    FILE* fout = NULL;
+
+    // Variables
+    char sname[10];
+    int jpegc = 0;
+    
+    // Buffer of 512 bytes 
+    BYTE buffer[512];
+    
+    // Untill end of file is reached
+    while (fread(buffer, sizeof(buffer), 1, fp) > 0)  
+    {
+        // Check the first 4 bytes for JPEG signature
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) & (buffer[2] == 0xff) && (buffer[3] == 0xe0 || buffer[3] == 0xe1))
+        {
+          // If already exist, then close the file
+          if (fout != NULL)
+          {
+              fclose(fout);
+          }
+         
+          // Name the files with three digits
+          sprintf(sname, "%03d.jpg", jpegc);
+          
+          // Open the file
+          fout = fopen(sname, "w");
+          
+          jpegc++;
+          
+          // Write into the file
+          fwrite(buffer, sizeof(buffer), 1, fout);
+         
+        } 
+        
+        else if (jpegc > 0)
+        {
+          // Put the JPEG into fout
+          fwrite(buffer, sizeof(buffer), 1, fout);
+        }
+        
+    } 
+    
+    // Close the file
+    fclose(fp);
+   
+    // That's all folks
+    return 0;
 }
